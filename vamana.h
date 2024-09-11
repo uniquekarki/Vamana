ca2bc3aa3161333957ea378ba542c6ab3adc0320
uniquekarki <unikarki@gmail.com>
2024-09-11 12:54:21 +0545

    Added code to get random graph structure from vector data points

--- vamana.h
+++ vamana.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cmath>
 #include <limits>
+#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
@@ -30,4 +32,27 @@ vector<float> calc_medoid(const vector<vector<float>> &data){
         };
     };
     return medoid;
+};
+
+vector<vector<int>> create_graph(const vector<vector<float>> &P, int R){
+    vector<vector<int>> G;
+    int n = P.size();
+
+    srand(time(0));
+    for (int i=0; i<n; i++){
+        vector<int> edges;
+        while (edges.size() < R){
+
+            // The random integer created would be the random edge for vector in i-th position
+            int randomNum = rand() % n;
+            
+            // Avoid self and duplicates
+            if(randomNum!=i && find(edges.begin(), edges.end(), randomNum) == edges.end() ){
+                edges.push_back(randomNum);
+            };
+        };
+        G.push_back(edges);
+    };
+
+    return G;
 };
