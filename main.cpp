f052f367d9ad8069bd4effbd707ea9533dd81e49
uniquekarki <unikarki@gmail.com>
2024-09-11 22:30:35 +0545

    Called the function to construct graph and get k-nearest neighbor from point xq

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <unordered_set>
 #include "vamana.h"
 using namespace std;
 
@@ -37,17 +38,34 @@ int main(){
     const string filename = "data.txt";
     vector<vector<float>> data = getData(filename);
 
-    printData(data);
+    // printData(data);
 
     // Calculate the medoid
     vector<float> medoid = calc_medoid(data);
 
     // Print the medoid
-    cout << "Medoid: ";
-    for (const auto &val : medoid) {
-        cout << val << " ";
-    }
-    cout << endl;
+    // cout << "Medoid: ";
+    // for (const auto &val : medoid) {
+    //     cout << val << " ";
+    // }
+    // cout << endl;
     
+    vector<vector<int>> graph = create_graph(data, 3);
+    vector<float> xq = {0.123123,0.123123};
+    pair<vector<int>, unordered_set<int>> result = greedy_search(data, graph, medoid, xq, 2, 3);
+
+    // Extract the nearest k elements and the set of visited nodes from the result
+    vector<int> nearest_k_elements = result.first;
+    unordered_set<int> visited_nodes = result.second;
+    cout << "Nearest K elems: \n";
+    for (const auto &elem: nearest_k_elements){
+        cout << elem << " ";
+    };
+
+    cout << "\nVisited Nodes: \n";
+    for (const auto &elem: visited_nodes){
+        cout << elem << " ";
+    };
+
     return 0;
 }
